97518db19ebed0319916159d4fbba089baffb1de
ALin <alin.rautoiu@gmail.com>
2016-09-17 00:51:32 +0300

    Selector pentru post

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser')
 
 var client = wordpress.createClient({
     url: "bdtk.ro",
-    username: "",
-    password: ""
+    username: "alin",
+    password: "6bsY2(WGBQGOfl@BeUn1LWFb"
 });
 
 var post =   { 
@@ -33,35 +33,32 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // client.newPost(post,function( error, posts ) {
 //     console.log(posts);
 // });
-var posts = [];
-// client.getPosts({'type':'encyclopedia'}, function(error, data) {
-//     //console.log(data);
-//     data.reduce(function(posts, cur){
-//         posts.push({'id': cur.id, 'title': cur.title});
-//         return posts;
-//     }, posts)
 
-//     console.log(posts);
-// });
+var posts = [{"id": "1", "title": "Test"}, {"id": "2", "title": "Test 2"}];
 
-for (var i =0; i < 500; i++) {
-    client.getPost(299, function(error, data) {
-        console.log(data);    
-    });
-}
-
+// function getPost(i) {
+//     client.getPost(i, function(error, data) {    
+//         if (data != undefined) {
+//             console.log("title: " + data.title);
+//             console.log("type: " + data.type);
+//             console.log();
+//         }    
+//         getPost(i + 1);
+//     });
+// }
 
 app.get('/', function(req, res) {
+    res.locals.posts = posts;
     res.render('form');         
 });
 
-app.post('/', jsonParser, function(req, res){
+app.post('/', function(req, res){
     console.log(req.body);
-    console.log(req.add);
 }) 
 
 var server = app.listen(process.env.PORT || 3002);
